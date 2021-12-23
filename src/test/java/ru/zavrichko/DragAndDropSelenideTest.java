38446f55c05c20d96be087070ef5152f192fcb49
a.zavrichko <andrey@zavrichko.dev>
2021-12-23 10:20:47 +0200

    add assertions element B on dragAndDropSelenideTest

--- src/test/java/ru/zavrichko/DragAndDropSelenideTest.java
+++ src/test/java/ru/zavrichko/DragAndDropSelenideTest.java
@@ -12,6 +12,8 @@ public class DragAndDropSelenideTest {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         // Проверяем что первый элемент div со значением A
         $$("#columns > div").first().shouldHave(text("A"));
+        // Проверяем что второй элемент div со значением B
+        $$("#columns > div").get(1).shouldHave(text("B"));
         // Находим первый элемент и перемещаем на второй
         $("#column-a").dragAndDropTo($("#column-b"));
         // Проверяем что элемент переместился и теперь на первом месте значение B
