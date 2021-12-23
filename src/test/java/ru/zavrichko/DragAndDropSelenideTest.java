66544955b0a3fec4d1e61d433d059cec867626ca
a.zavrichko <andrey@zavrichko.dev>
2021-12-23 09:10:29 +0200

    add assertions on dragAndDropSelenideTest

--- src/test/java/ru/zavrichko/DragAndDropSelenideTest.java
+++ src/test/java/ru/zavrichko/DragAndDropSelenideTest.java
@@ -2,17 +2,19 @@ package ru.zavrichko;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDropSelenideTest {
     @Test
     void dragAndDropSelenideTest(){
         // Открываем страницу с drag and drop
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        // Проверяем что первый элемент div со значением A
+        $$("#columns > div").first().shouldHave(text("A"));
         // Находим первый элемент и перемещаем на второй
         $("#column-a").dragAndDropTo($("#column-b"));
-        // Проверяем что элемент переместился
-
+        // Проверяем что элемент переместился и теперь на первом месте значение B
+        $$("#columns > div").first().shouldHave(text("B"));
     }
 }
